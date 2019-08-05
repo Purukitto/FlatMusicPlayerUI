3643ab6e6a025d2b8c32723b70b432ce7d59b2da
Purukitto <purukitto@gmail.com>
2019-08-06 01:17:13 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttery_seekbar/fluttery_seekbar.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: Home(),
@@ -10,6 +12,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _thumbPercent = 0.4;
+
+  Widget _buildRadialSeek() {
+    return RadialSeekBar(
+      trackColor: Colors.white.withOpacity(.5),
+      trackWidth: 2.0,
+      progressColor: Colors.white,
+      progressWidth: 5.0,
+      thumbPercent: _thumbPercent,
+      thumb: CircleThumb(
+        color: Colors.white,
+        diameter: 20.0,
+      ),
+      progress: _thumbPercent,
+      onDragUpdate: (double percent) {
+        setState(() {
+          _thumbPercent = percent;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -24,8 +48,64 @@ class _HomeState extends State<Home> {
           ),
           onPressed: () {},
         ),
+        title: Text("Now Playing",
+            style: TextStyle(color: Colors.white, fontFamily: "Nexa")),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
       ),
-      body: Container(),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Container(
+              width: 250,
+              height: 250,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(.5),
+                        shape: BoxShape.circle),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: _buildRadialSeek(),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      child: ClipOval(
+                        clipBehavior: MClipper(),
+                        child: Image.asset(
+                          "ditf.jpg",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
+  }
+}
+
+class MClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromCircle(
+        center: Offset(size.width / 2, size.height / 2),
+        radius: min(size.width, size.height) / 2);
   }
 }
